fc50fb22630e1d8cc011f6e3045140e499973c9a
gzavvo <gzavvo@gmail.com>
2020-12-31 18:51:08 +0100

    2.4: Course information step9

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -6,8 +6,12 @@ const Total = ({ course }) => {
     .map(part => part.exercises)
     .reduce((acc, part) => acc + part)
 
+  const pStyle = {
+    fontWeight: 'bold'
+  };
+
   return (
-    <p>
+    <p style={pStyle}>
       total of {total} exercises
     </p>
   )
@@ -48,29 +52,56 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      { 
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      }, 
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        { 
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        }, 
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id:1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id:2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+
+  return (
+    <>
+    {
+      courses.map(course => {
+        return <Course key= {course.id} course={course} />
+      })
+    }
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
