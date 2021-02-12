f8fd9a9368e8a1a55fbe84f8cd6bde2e0b57c9a6
gzavvo <gzavvo@gmail.com>
2021-02-12 13:23:51 +0200

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 
 function App() {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' },
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-123456' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -15,14 +19,20 @@ function App() {
     }
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,6 +46,12 @@ function App() {
           />
         </div>
         <div>
+          number: <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
@@ -44,7 +60,10 @@ function App() {
         <tbody>
           {
             persons.map(person => {
-              return <tr key={person.name}><td>{person.name}</td></tr>
+              return <tr key={person.name}>
+                  <td>{person.name}</td>
+                  <td>{person.number}</td>
+                </tr>
             })
           }
         </tbody>
